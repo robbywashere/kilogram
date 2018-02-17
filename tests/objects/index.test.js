24b98da9af70dd3425f0b398a4f821cae4b9fde0
robbywashere <robertapolana@gmail.com>
2018-02-17 15:46:01 +0700

    added better test for object loader

--- tests/objects/index.test.js
+++ tests/objects/index.test.js
@@ -1,25 +1,29 @@
 
 const { loadObject, initObjects, newRegistry } = require('../../server-lib/objectLoader');
-const { STRING, INTEGER } = require('sequelize');
+
+const { STRING, INTEGER, Model } = require('sequelize');
 const DBSync = require('../../db/sync');
 const assert = require('assert');
 
 
 describe('objectLoader', function(){
+  let InitObjs = {};
   beforeEach(()=>DBSync(true));
-  it ('should load an object',async function(){
-
+    
     const testObj = {
       Name: 'TestObj',
       Properties:{
         foo: {
           type: STRING,
+          permit: true
         },
         bar: {
+          omit: true,
           type: STRING,
         },
         UserId: {
-          type: INTEGER
+          type: INTEGER,
+          omit: true
         }
       },
       ScopeFunctions: true, 
@@ -31,40 +35,85 @@ describe('objectLoader', function(){
       Hooks: {
       },
       Methods:{
+        instanceMeth: function (){ return true },
       },
       StaticMethods: {
+        staticMeth: function (){ return true },
       },
-      Init(){
+      Init(objs){
+        InitObjs = objs;
       },
     };
     const registry = newRegistry();
     loadObject(testObj,registry)
     initObjects(registry);
+    const { TestObj } = registry.objects;
+
+  it ('should call Init() function with registry', function(){
+    assert(InitObjs.TestObj.prototype instanceof Model)
+  })
+
+  it ('should make instance and static methods', async function(){
+    assert(TestObj.staticMeth())
+    const to = await TestObj.create({});
+    assert(to.instanceMeth())
+  })
+
+  it ('should turn scopes into functions with ScopeFunctions: true', async function(){
+  
+    const to1 = await TestObj.create({ UserId: 1 });
+    const to2 = await TestObj.create({ UserId: 2 });
+
+    const userScopedTo = await TestObj.userScoped({ id: 2 });
+
+    assert.equal(userScopedTo.length,1)
+
+    assert.equal(userScopedTo[0].UserId, 2)
+
+    assert.equal( (await TestObj.userScoped({ id: 3 })).length, 0)
+  
+  })
+
+  it('should load an object',async function(){
 
     await DBSync(true);
-    const { TestObj } = registry.objects;
     await TestObj.create({ foo: 'bar', bar: 'foo', UserId: 1 });
     await TestObj.create({ foo: 'blah', bar: 'blah', UserId: 2 });
     const testobj = await TestObj.findById(1);
     assert(testobj);
-    //assert(TestObj.authorize('read',true));
     const empty = await TestObj.userScoped({ admin: false, id: 0 })
     assert.equal(empty.length, 0)
-
-      /* TODO: test me with new controller
-       *
-    const notEmpty = await TestObj.userScoped({ admin: false, id: 1 })
-    assert.equal(notEmpty.length, 1)
-    assert.equal(notEmpty[0].foo,'bar')
-    const ps0 = await TestObj.policyScope('all', { admin: false, id: 0 }).findOne()
-    assert.equal(ps0, null)
-    const ps1 = await TestObj.policyScope('all', { admin: false, id: 1 }).findOne()
-    assert(ps1)
-    assert.equal(ps1.foo, 'bar')
-    */
   })
 
 
+  it('should register omitted and permitted keys', function(){
+    assert.deepEqual(TestObj.permitted, { foo: true });
+    assert.deepEqual(TestObj.omitted, { bar: true, UserId: true });
+  })
+
+  it('should sanitize a JSON object based on permitted keys', function(){
+    assert.deepEqual(TestObj.sanitizeParams({ foo: 'bar', bar: 'foo'}),{ foo: 'bar' })
+  })
+  it('should set dataValues of object based on permitted keys', function(){
+    const to = TestObj.build({})
+    to.permittedSet({ foo: 'bar', bar: 'foo'});
+    assert.deepEqual(to.toJSON(),{ foo: 'bar', id: null  })
+  })
+  it('should safely retrieve keys based on omitted using _getSafe',async function(){
+    const to = TestObj.build({ foo: 'bar', bar: 'foo' });
+    await to.save();
+    assert.equal(to._getSafe('bar'),undefined);
+    assert.equal(to._getSafe('foo'),'bar');
+  });
+  it('should serialize object using omitted via _getSafe',async function(){
+    const to = TestObj.build({ foo: 'bar', bar: 'foo', UserId: 1 });
+    await to.save();
+    const toJSON = to.serialize();
+    assert.equal(toJSON.bar, undefined);
+    assert.equal(toJSON.UserId, undefined);
+    assert.equal(toJSON.foo, 'bar');
+    assert.deepEqual(Object.keys(toJSON),[ 'id', 'foo', 'updatedAt', 'createdAt' ]);
+  })
 })
 
 
