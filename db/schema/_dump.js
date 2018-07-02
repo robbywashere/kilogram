28ae6ec3b0c9d397cf6e2dc75108a1521b89ab96
robbywashere <robertapolana@gmail.com>
2018-07-02 22:52:32 +0700

    updated to sync force

--- db/schema/_dump.js
+++ db/schema/_dump.js
@@ -1,11 +1,12 @@
 const pgdump = require('./pg_dump_schema');
 const dbConfig = require('../config');
+const dbSync = require('../sync');
 const config = require('config');
 const path = require('path');
 
-const schemaPath = path.join(__dirname,`schema.snapshot.sql`);
-
-const { username, database } = dbConfig['development'];
-
-pgdump({ path: schemaPath, username, database });
-
+dbSync(true).then(()=>{
+  const schemaPath = path.join(__dirname,`schema.snapshot.sql`);
+  const { username, database } = dbConfig['development'];
+  pgdump({ path: schemaPath, username, database });
+  process.exit(0);
+})
