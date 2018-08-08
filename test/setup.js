9fa9bd3c9d883fc212d0e53563aebbd83e54a564
robbywashere <robertapolana@gmail.com>
2018-08-09 05:19:23 +0700

    test setup.js tests db connection

--- test/setup.js
+++ test/setup.js
@@ -1,13 +1,25 @@
-console.log('SETTING ENVIRONMENT TO TEST ENV');
 
-require('child_process').execFileSync('npm', ['run', 'e:test', 'db:schema:up'], {
-  stdio: 'ignore',
-});
 
-const { setLogLevel } = require('../lib/logger');
+const { logger } = require('../lib/logger');
+const chalk = require('chalk');
 
-NODE_CONFIG_ENV = 'test';
 process.env.NODE_ENV = 'test';
 const config = require('config');
 
 config.NODE_ENV = 'test';
+
+const db = require('../db');
+
+(async ()=>{
+  try {
+    await db.authenticate();
+  } catch(e) {
+    console.error(e);
+    console.error(chalk.red(`Could not authenticate with DB, insure DB instance is running`));
+    process.exit(1);
+  }
+  require('child_process').execFileSync('npm', ['run', 'e:test', 'db:schema:up'], {
+    stdio: 'ignore',
+  });
+})()
+
